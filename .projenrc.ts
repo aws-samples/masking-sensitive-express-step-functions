1f408bac1d8c70492c3e7e1c6d5368695b10345c
Luis Morales <moralesl@users.noreply.github.com>
2024-06-20 11:22:31 +0200

    Update CDK -> v2.146.0

--- .projenrc.ts
+++ .projenrc.ts
@@ -1,7 +1,7 @@
 import { awscdk } from 'projen';
 
 const project = new awscdk.AwsCdkTypeScriptApp({
-  cdkVersion: '2.139.0',
+  cdkVersion: '2.146.0',
   defaultReleaseBranch: 'main',
   name: 'logging-and-masking-express-step-functions',
   projenrcTs: true,
